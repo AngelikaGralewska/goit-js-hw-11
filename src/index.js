08b364a513c930831a3fdb24eb7f670995f29b19
AngelikaGralewska <angelika.gralewska@gmail.com>
2022-11-02 21:57:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -104,10 +104,10 @@ function renderGallery(images) {
           <div class="photo-card" id="${id}">
             <img src="${webformatURL}" alt="${tags}" loading="lazy" />
             <div class="info">
-              <p class="info-item"><b>Likes</b>: ${likes}</p>
-              <p class="info-item"><b>Views</b>: ${views}</p>
-              <p class="info-item"><b>Comments</b>: ${comments}</p>
-              <p class="info-item"><b>Downloads</b>: ${downloads}</p>
+              <p class="info-item">Likes: ${likes}</p>
+              <p class="info-item">Views: ${views}</p>
+              <p class="info-item">Comments: ${comments}</p>
+              <p class="info-item">Downloads: ${downloads}</p>
             </div>
           </div>
         </a>
